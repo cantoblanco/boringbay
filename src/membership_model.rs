4da45aa08474988c3656550e54abb1ba1f909e91
naiba <hi@nai.ba>
2022-05-08 11:43:12 +0800

    🐛 skip_deserializing

--- src/membership_model.rs
+++ src/membership_model.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Clone, Serialize)]
 pub struct Membership {
+    #[serde(skip_deserializing)]
     pub id: i64,
     pub domain: String,
     pub name: String,
